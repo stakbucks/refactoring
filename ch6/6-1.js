52ef64097cd92ff4578983e6cee9411a7b0b6cf8
stakbucks <dhassidu11@gmail.com>
2023-07-05 21:07:05 +0900

    6.1 완료

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,31 +1,68 @@
-export function printOwing(invoice) {
-  let outstanding = 0;
+// export function printOwing(invoice) {
+//   let outstanding = 0;
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+//   console.log("***********************");
+//   console.log("**** Customer Owes ****");
+//   console.log("***********************");
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+//   // calculate outstanding
+//   for (const o of invoice.orders) {
+//     outstanding += o.amount;
+//   }
 
-  // record due date
+//   // record due date
+//   const today = new Date();
+//   invoice.dueDate = new Date(
+//     today.getFullYear(),
+//     today.getMonth(),
+//     today.getDate() + 30
+//   );
+
+//   //print details
+//   console.log(`name: ${invoice.customer}`);
+//   console.log(`amount: ${outstanding}`);
+//   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+// }
+
+const invoice = {
+  orders: [{ amount: 2 }, { amount: 5 }],
+  customer: "엘리",
+};
+printOwing(invoice);
+
+function printOwing(invoice) {
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+}
+
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => sum + order.amount, 0);
+  // let result = 0;
+  // for (const o of invoice.orders) {
+  //   result += o.amount;
+  // }
+  // return result;
+}
+function recordDueDate(invoice) {
+  // 지급날짜를 계산
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
-
-  //print details
+}
+function printDetails(invoice, outstanding) {
+  // 세부사항을 출력!
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
-
-const invoice = {
-  orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
-};
-printOwing(invoice);
